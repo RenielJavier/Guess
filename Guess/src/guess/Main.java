901d2c4f3c8e99608c6a62f7179bfc4d5888768c
unknown <javier.ren.2004@IAV.IT>
2022-05-10 10:48:16 +0200

    seconda versione

--- Guess/src/guess/Main.java
+++ Guess/src/guess/Main.java
@@ -4,11 +4,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Programma avviato!");
-		Tree t =new Tree(new Node("Io sono la radice"));
-		
-		t.add("primo nodo");
-		t.add("secondo nodo");
-		
+		Tree t =new Tree(new Node("A"));
+		t.addLeft(new Node("b"));
+		t.addRight(new Node("c"));
+		t.goLeft();
+		t.addLeft(new Node("d"));
+		t.goRoot();
+		t.goRight();
+		t.addRight(new Node("f"));
+		t.addLeft(new Node("e"));
+		t.goLeft();
+		t.addRight(new Node("h"));
+		t.addLeft(new Node("g"));
 		System.out.println(t);
 		
 	}
--- Guess/src/mycontainers/Tree.java
+++ Guess/src/mycontainers/Tree.java
@@ -14,9 +14,9 @@ public class Tree {
 	
 	/*Constructors*/
 	public Tree() {
-		current=root=null;
+		Node n= new Node("Uknown");
+		root=current=n;
 	}
-	
 	public Tree(Node root) {
 		this.current=this.root=root;
 	}
@@ -28,15 +28,48 @@ public class Tree {
 		}
 	}
 	
-	public void add(String value) {
-		Node nuovo=new Node(value);
-		if(root==null) {
-			root=current=nuovo;
-		}else{}
-		nuovo.setLeft(root);
-		root=nuovo;
+	public void addLeft(Node n) {
+		if(current==null) {
+			return;
+		}
+		if(current.getLeft()==null) {
+			current.setLeft(n);
+			n.setParent(current);
+		}
 	}
-
+	public void addRight(Node n) {
+		if(current==null) {
+			return;
+		}
+		if(current.getRight()==null) {
+			current.setRight(n);
+			n.setParent(current);
+		}
+	}
+	
+	public Node goLeft(){
+		if(current!=null && current.getLeft()!=null) {
+			current=current.getLeft();
+		}
+		return current;
+	}
+	public Node goRight(){
+		if(current!=null && current.getRight()!=null) {
+			current=current.getRight();
+		}
+		return current;
+	}
+	public Node goParent(){
+		if(current!=null && current.getParent()!=null) {
+			current=current.getParent();
+		}
+		return current;
+	}
+	
+	public Node goRoot() {
+		return current=root;
+	}
+	
 	@Override
 	public String toString() {
 		return printMe(root,"");
